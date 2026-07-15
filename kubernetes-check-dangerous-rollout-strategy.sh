adaa211e8b37a52e5e7cdc276a31387c555ac5bd
dugganjl <dugganjl@gmail.com>
2026-07-15 16:27:20 -0700

    Add files via upload

---
+++ kubernetes-check-dangerous-rollout-strategy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Check for dangerous rollout strategy
+read -r -p "Enter the path to the deployment.yaml file: " DEPLOYMENT_YAML
+echo
+MAX_UNAVAIL=$(yq '.spec.strategy.rollingUpdate.maxUnavailable | select(. != null)' $DEPLOYMENT_YAML)
+echo "rollingUpdate.maxUnavailable = $MAX_UNAVAIL"
+echo
+if [[ "$MAX_UNAVAIL" == "100%" ]] || [[ "$MAX_UNAVAIL" -ge $(yq '.spec.replicas | select(. != null)' $DEPLOYMENT_YAML) ]]; then
+    echo "WARNING: Dangerous maxUnavailable detected!"
+    exit 1
+else
+    echo "No dangerous rollout strategy detected."
+fi
